668952dfd365a1c0ccad95f899ef3d106e8c4ba9
okeefj22 <jacob.okeeffe22@mail.dcu.ie>
2016-12-02 18:19:54 +0000

    Copied required files from assignment 1.

---
+++ build.sh
@@ -0,0 +1,11 @@
+javacc CCALParser.jj
+javac CCALParser.java
+
+java CCALParser ccal/case-insensitive-01.ccl
+java CCALParser ccal/case-insensitive-02.ccl
+java CCALParser ccal/case-insensitive-03.ccl
+java CCALParser ccal/comments-01.ccl
+java CCALParser ccal/functions-01.ccl
+java CCALParser ccal/functions-02.ccl
+java CCALParser ccal/scopes-01.ccl
+
